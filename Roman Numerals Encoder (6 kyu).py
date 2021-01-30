c5ac041a529b2757c1e840964b5c7ece9582aef0
arsestor <arsestor@gmail.com>
2021-01-30 19:57:16 +0500

    Completed! (02)

--- Roman Numerals Encoder (6 kyu).py
+++ Roman Numerals Encoder (6 kyu).py
@@ -1,5 +1,5 @@
 def solution(n):
-    ones = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'XI']
+    ones = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX']
     tens = ['', 'X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC']
     hundreds = ['', 'C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM']
     thousands = ['', 'M', 'MM', 'MMM']
