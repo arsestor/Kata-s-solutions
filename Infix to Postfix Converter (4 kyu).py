6df64de56b22523d8d718bbc940a7198933214fb
arsestor <arsestor@gmail.com>
2021-03-24 12:55:29 +0500

    Completed! (02)

--- Infix to Postfix Converter (4 kyu).py
+++ Infix to Postfix Converter (4 kyu).py
@@ -1,17 +1,7 @@
 def to_postfix(infix):
     stack = []
     postfix = ''
-
-    def preced(i):
-        if i == '+' or i == '-':
-            return 1
-        elif i == '*' or i == '/':
-            return 2
-        elif i == '^':
-            return 3
-        else:
-            return 0
-
+    p = {'(':0,'+':1,'-':1,'*':2,'/':2,'^':3}
     for i in infix:
         if i.isdigit():
             postfix += i
@@ -24,7 +14,7 @@ def to_postfix(infix):
                 postfix += stack.pop(0)
             stack.remove('(')
         else:
-            while len(stack)!=0 and preced(i)<=preced(stack[0]):
+            while len(stack)!=0 and p[i]<=p[stack[0]]:
                 postfix += stack.pop(0)
             stack.insert(0, i)
     while len(stack)!=0:
